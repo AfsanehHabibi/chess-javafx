1f8ef55fce15857024e7aef44bbcae1329423b10
Afsaneh Habibi <a.habibi1394@gmail.com>
2023-06-29 13:51:44 +0330

    Update reference path to fxml files

--- src/main/java/sample/FatherController.java
+++ src/main/java/sample/FatherController.java
@@ -9,7 +9,7 @@ import static sample.Main.stage;
 public class FatherController {
     public void loadPage(String name){
         try {
-            Parent root = FXMLLoader.load(getClass().getResource(name+".fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/fxml/"+name+".fxml"));
             stage.setScene(new Scene(root, 600, 600));
         } catch (Exception e) {
             e.printStackTrace();
