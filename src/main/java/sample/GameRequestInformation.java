eed75088e752f8860d22ad7a2e6c70f616d289de
Afsaneh Habibi <a.habibi1394@gmail.com>
2023-07-07 12:39:40 +0330

    Rename Player class to User Update its usages

--- src/main/java/sample/GameRequestInformation.java
+++ src/main/java/sample/GameRequestInformation.java
@@ -3,7 +3,7 @@ package sample;
 import java.io.Serializable;
 
 public class GameRequestInformation implements Serializable{
-    Player seek_player;
+    User seek_player;
     int client_id;
     boolean rated;
     boolean isTime;
@@ -11,13 +11,12 @@ public class GameRequestInformation implements Serializable{
     Clock clock;
     int player_code;
 
-    public Player getSeek_player() {
+    public User getSeek_player() {
         return seek_player;
     }
 
-
-    public GameRequestInformation(Player seek_player, int client_id, boolean rated, boolean isTime, Color color,
-            int player_code,String time) {
+    public GameRequestInformation(User seek_player, int client_id, boolean rated, boolean isTime, Color color,
+                                  int player_code, String time) {
         this.seek_player = seek_player;
         this.client_id = client_id;
         this.rated = rated;
