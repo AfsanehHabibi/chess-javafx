9d1419fb9e84a24a52beaf5ec6885c57945e25ad
Afsaneh Habibi <a.habibi1394@gmail.com>
2023-07-07 12:56:34 +0330

    Add RandoopSeedGenerator and side effect free methods for Randoop usage

---
+++ src/main/java/sample/game/logic/RandoopSeedGenerator.java
@@ -0,0 +1,38 @@
+package sample.game.logic;
+
+import randoop.*;
+import sample.Color;
+
+public class RandoopSeedGenerator {
+    @TestValue
+    public static int i0 = 0;
+
+    @TestValue
+    public static int i1 = 1;
+
+    @TestValue
+    public static int i2 = 2;
+
+    @TestValue
+    public static int i3 = 3;
+
+    @TestValue
+    public static int i4 = 4;
+
+    @TestValue
+    public static int i5 = 5;
+
+    @TestValue
+    public static int i6 = 6;
+
+    @TestValue
+    public static int i7 = 7;
+
+    public Color getBlackColor() {
+        return Color.Black;
+    }
+
+    public Color getWhiteColor() {
+        return Color.White;
+    }
+}
